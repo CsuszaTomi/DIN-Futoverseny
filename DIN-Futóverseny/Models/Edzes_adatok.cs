d966b55da4f170e973067dd2a64144b9624c27f6
Imre <nemethi@kkszki.hu>
2026-01-12 17:54:02 +0100

    Valami elkezdődött!  :-). A bekérések megvalósítása

---
+++ DIN-Futóverseny/Models/Edzes_adatok.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIN_Futóverseny.Models
+{
+    internal class Edzes_adatok
+    {
+        private DateTime datum;
+        private decimal tavolsag;
+        private TimeSpan idotartam;
+        private int max_pulzus;
+
+        public DateTime Datum { get => datum; set => datum = value; }
+        public decimal Tavolsag { get => tavolsag; set => tavolsag = value; }
+        public TimeSpan Idotartam { get => idotartam; set => idotartam = value; }
+        public int Max_pulzus { get => max_pulzus; set => max_pulzus = value; }
+
+        public Edzes_adatok(DateTime datum, decimal tavolsag, TimeSpan idotartam, int max_pulzus)
+        {
+            this.Datum = datum;
+            this.Tavolsag = tavolsag;
+            this.Idotartam = idotartam;
+            this.Max_pulzus = max_pulzus;
+        }
+    }
+}
--- DIN-Futóverseny/Program.cs
+++ DIN-Futóverseny/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DIN_Futóverseny.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,35 @@ namespace DIN_Futóverseny
 {
     internal class Program
     {
+
+        static List<Edzes_adatok> adatok = new List<Edzes_adatok>();
         static void Main(string[] args)
         {
+            // a menüt is majd meg kell csinálni!
+            try
+            {
+                Console.WriteLine("Felhasználó adatatinak felvitele!: ");
+                Console.WriteLine("Kérem a dátumot (yyyy-mm-dd) formátumban: ");
+                DateTime datum = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Kérem a tavolságot: ");
+                decimal tav=decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("Kérem az időtatamot (óó:pp:mm) formátumban: ");
+                TimeSpan idotartam=TimeSpan.Parse(Console.ReadLine());
+
+                Console.WriteLine("Kérem a maximális pulzus adatot: ");
+                int m_pulzus=int.Parse(Console.ReadLine());
+
+                Edzes_adatok adat = new Edzes_adatok(datum,tav,idotartam,m_pulzus);
+                adatok.Add(adat);
+            }
+            catch  (Exception e)
+            {
+                Console.WriteLine("Hiba történt: ", e);
+            }
+
+            
 
         }
     }
